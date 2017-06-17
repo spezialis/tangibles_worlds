456574d92ad2413ebd47d05476224d1df78713c7
stella <stella.speziali@ecal.ch>
2017-06-17 22:50:52 +0200

    Wire world light random position and color

---
+++ Assets/_Assets/_Scrpits/MoveLight.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening; 
+
+public class MoveLight : MonoBehaviour {
+
+	// TODO proposition: maybe the light could follow the camera rotation, and stay at a specific distance
+	Light light;
+
+	public float duration;
+	float startTime;
+
+	// Position
+	float xStart;
+	float yStart;
+	float zStart;
+
+	float xTarget;
+	float yTarget;
+	float zTarget;
+
+	float rangePosition = 0.5f;
+
+	// Color
+	public Color[] randomColor;
+	int startColor;
+	int targetColor;
+
+	// TODO change intensity or range or both
+
+	void Start () {
+		light = GetComponent<Light> ();
+
+		xTarget = 0;
+		yTarget = 0;
+		zTarget = 0;
+		AssignNewTargets ();
+
+		startColor = getRandomColor();
+		light.color = randomColor[startColor];
+	}
+
+	void AssignNewTargets() {
+		startTime = Time.time;
+
+		// Position
+		xStart = xTarget;
+		yStart = yTarget;
+		zStart = zTarget;
+
+		xTarget = Random.Range (-rangePosition, rangePosition);
+		yTarget = Random.Range (-rangePosition, rangePosition);
+		zTarget = Random.Range (-rangePosition, rangePosition);
+
+		// Color
+		startColor = targetColor;
+		targetColor = getRandomColor();
+	}
+		
+	public int getRandomColor(){
+		return Random.Range(0, (randomColor.Length - 1));
+	}
+		
+	void Update () {
+		// Random range with duration
+		float completion = (Time.time - startTime) / duration;
+
+		// Position
+		float xLerped = Mathf.Lerp(xStart, xTarget, completion);
+		float yLerped = Mathf.Lerp(yStart, yTarget, completion);
+		float zLerped = Mathf.Lerp(zStart, zTarget, completion);
+
+		light.transform.localPosition = new Vector3(xLerped, yLerped, zLerped);
+
+		// Color
+		Color lerpedColor = Color.Lerp (randomColor [startColor], randomColor [targetColor], completion);
+		light.color = lerpedColor;
+
+		if (completion >= 1) {
+			AssignNewTargets ();
+		} 
+	}
+}
--- Assets/_Assets/_Scrpits/WireWorldManager.cs
+++ Assets/_Assets/_Scrpits/WireWorldManager.cs
@@ -11,6 +11,8 @@ public class WireWorldManager : WorldManager {
 //	public Animation wallWire3;
 //	public Animation wallWire4;
 
+	// TODO at the end of the project remove the debugAnimateDisplacement
+	public bool debugAnimateDisplacement;
 	public MegaDisplace cut;
 	public MegaDisplace cut1;
 	public MegaDisplace cut2;
@@ -19,50 +21,59 @@ public class WireWorldManager : WorldManager {
 	public override void SetWallState( int index, bool active ) {
 		switch (index) {
 		case 1:
-			// Animate displacement
-			cut.amount += 0.001f;
+			if (debugAnimateDisplacement) {
+				// Animate displacement
+				cut.amount += 0.001f;
 
-			if (cut.amount >= 0.01f) {
-				cut.amount = 0.01f;
+				if (cut.amount >= 0.01f) {
+					cut.amount = 0.01f;
 
-				// PingPong lerp
-				cut.offset = new Vector2(Mathf.PingPong(Time.time, 3), Mathf.PingPong(Time.time, 3));
+					// PingPong lerp
+					cut.offset = new Vector2(Mathf.PingPong(Time.time, 3), Mathf.PingPong(Time.time, 3));
+				}
 			}
+
 			break;
 
 		case 2:
-			// Animate displacement
-			cut1.amount += 0.001f;
+			if (debugAnimateDisplacement) {
+				// Animate displacement
+				cut1.amount += 0.001f;
 
-			if (cut1.amount >= 0.01f) {
-				cut1.amount = 0.01f;
+				if (cut1.amount >= 0.01f) {
+					cut1.amount = 0.01f;
 
-				// PingPong lerp
-				cut1.offset = new Vector2(Mathf.PingPong(Time.time, 3), Mathf.PingPong(Time.time, 3));
+					// PingPong lerp
+					cut1.offset = new Vector2 (Mathf.PingPong (Time.time, 3), Mathf.PingPong (Time.time, 3));
+				}
 			}
 			break;
 
 		case 3:
-			// Animate displacement
-			cut2.amount += 0.001f;
+			if (debugAnimateDisplacement) {
+				// Animate displacement
+				cut2.amount += 0.001f;
 
-			if (cut2.amount >= 0.01f) {
-				cut2.amount = 0.01f;
+				if (cut2.amount >= 0.01f) {
+					cut2.amount = 0.01f;
 
-				// PingPong lerp
-				cut2.offset = new Vector2(Mathf.PingPong(Time.time, 3), Mathf.PingPong(Time.time, 3));
+					// PingPong lerp
+					cut2.offset = new Vector2 (Mathf.PingPong (Time.time, 3), Mathf.PingPong (Time.time, 3));
+				}
 			}
 			break;
 
 		case 4:
-			// Animate displacement
-			cut3.amount += 0.001f;
+			if (debugAnimateDisplacement) {
+				// Animate displacement
+				cut3.amount += 0.001f;
 
-			if (cut3.amount >= 0.01f) {
-				cut3.amount = 0.01f;
+				if (cut3.amount >= 0.01f) {
+					cut3.amount = 0.01f;
 
-				// PingPong lerp
-				cut3.offset = new Vector2(Mathf.PingPong(Time.time, 3), Mathf.PingPong(Time.time, 3));
+					// PingPong lerp
+					cut3.offset = new Vector2 (Mathf.PingPong (Time.time, 3), Mathf.PingPong (Time.time, 3));
+				}
 			}
 			break;
 		default:
